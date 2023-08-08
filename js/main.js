c3c92d1b6558f55bbdbbcfeb49cf53fe3127f8c2
NgocQuy1706 <103399772+NgocQuy1706@users.noreply.github.com>
2023-08-08 10:03:17 +0700

    create source

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//đoạn mã sử dụng để giữ thanh navbar và chỉ cần tải lại phần nội dung không cần phải tải lại cả trang
+document.addEventListener("DOMContentLoaded", function() {
+    // Tải thanh navbar
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            var navbarDiv = document.getElementById("navbar");
+            navbarDiv.innerHTML = data;
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+
+    var links = document.querySelectorAll("#navbar a");
+    links.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            var targetPage = this.getAttribute("href");
+            loadPage(targetPage);
+        });
+    });
+
+    function loadPage(page) {
+        var contentDiv = document.getElementById("content");
+        
+        // Tải nội dung trang
+        fetch(page)
+            .then(response => response.text())
+            .then(data => contentDiv.innerHTML = data)
+            .catch(error => console.error("Error loading page:", error));
+    }
+});
