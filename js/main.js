697843d5a02b06b85e3cfc8a3c62f2c5bcd2393a
NgocQuy1706 <103399772+NgocQuy1706@users.noreply.github.com>
2023-08-09 10:25:08 +0700

    create footer and navbar

--- js/main.js
+++ js/main.js
@@ -28,3 +28,18 @@ document.addEventListener("DOMContentLoaded", function() {
             .catch(error => console.error("Error loading page:", error));
     }
 });
+
+
+//đoạn mã sử dụng để giữ phần footer và chỉ cần tải lại phần nội dung không cần phải tải lại cả trang
+document.addEventListener("DOMContentLoaded", function() {
+    // Tải phần footer
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            var footerDiv = document.getElementById("footer");
+            footerDiv.innerHTML = data;
+        })
+        .catch(error => console.error("Error loading footer:", error));
+
+    // Các mã xử lý khác ở đây
+});
